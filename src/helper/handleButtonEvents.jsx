5e19c36ec50b0e04f6a692e3c1907b22462170b9
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-03 18:56:30 +0530

    error solving

--- src/helper/handleButtonEvents.jsx
+++ src/helper/handleButtonEvents.jsx
@@ -1,17 +1,20 @@
-import React from 'react';
-import isValidPhoneNumber from '../utils/validation';
+import React from "react";
+import isValidPhoneNumber from "../utils/validation";
 
 const sendWhatsAppMessage = (phoneNumber, message) => {
   if (!isValidPhoneNumber(phoneNumber)) {
     console.error("Invalid phone number");
     return;
   }
-  const url = `https://wa.me/${phoneNumber.replace("+", "")}?text=${encodeURIComponent(message)}`;
+  const url = `https://wa.me/${phoneNumber.replace(
+    "+",
+    ""
+  )}?text=${encodeURIComponent(message)}`;
   window.open(url, "_blank");
 };
 
 const handleWhatsAppMessage = () => {
-  const phoneNumber = process.env.REACT_APP_WHATSAPP_NUMBER || "919978764266"; // Fallback to hardcoded number
+  const phoneNumber = process.env.REACT_APP_WHATSAPP_NUMBER || "919978764266";
   const message = "Hello! I'm interested in your English classes.";
   sendWhatsAppMessage(phoneNumber, message);
 };
@@ -22,10 +25,12 @@ const handleEnrollNow = () => {
 };
 
 const renderButton = (text, onClick, icon, isWhatsapp = false) => {
-  let buttonClass = "bg-blue-600 hover:bg-blue-700 rounded-full text-white font-bold py-3 px-6 flex items-center justify-center focus:outline-none focus:shadow-outline";
-  
+  let buttonClass =
+    "bg-blue-600 hover:bg-blue-700 rounded-full text-white font-bold py-3 px-6 flex items-center justify-center focus:outline-none focus:shadow-outline";
+
   if (isWhatsapp) {
-    buttonClass = "bg-green-700 hover:bg-green-800 rounded-full text-white font-bold py-3 px-6 flex items-center justify-center focus:outline-none focus:shadow-outline";
+    buttonClass =
+      "bg-green-700 hover:bg-green-800 rounded-full text-white font-bold py-3 px-6 flex items-center justify-center focus:outline-none focus:shadow-outline";
   }
 
   return (
@@ -36,4 +41,9 @@ const renderButton = (text, onClick, icon, isWhatsapp = false) => {
   );
 };
 
-export { handleEnrollNow, handleWhatsAppMessage, sendWhatsAppMessage, renderButton };
+export {
+  handleEnrollNow,
+  handleWhatsAppMessage,
+  sendWhatsAppMessage,
+  renderButton,
+};
