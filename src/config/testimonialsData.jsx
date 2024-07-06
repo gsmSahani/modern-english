447c28bbc92b57906052a8e3357cd39b79d30719
gsmSahani <sahanigautam0446162@gmail.com>
2024-07-06 11:04:33 +0530

    testimonials added

--- src/config/testimonialsData.jsx
+++ src/config/testimonialsData.jsx
@@ -5,6 +5,15 @@ import shivani from "/assets/image/testimonials/Shivani_Bhanushali.webp";
 import sagar from "/assets/image/testimonials/sagar.webp";
 import krisha from "/assets/image/testimonials/Krisha_Patel.webp";
 import preyash from "/assets/image/testimonials/Preyash_Patel.webp";
+
+import dhrasti from "/assets/image/testimonials/dhrasti_patel.webp";
+import feni from "/assets/image/testimonials/feni.webp";
+import hemanshi from "/assets/image/testimonials/hemanshi_prajapati.webp";
+import nilesh from "/assets/image/testimonials/nilesh_patel.webp";
+import jayshree from "/assets/image/testimonials/disha_patel.webp";
+import yogesh from "/assets/image/testimonials/yogesh_patel.webp";
+import yug from "/assets/image/testimonials/yug.webp";
+import malik from "/assets/image/testimonials/muaja_malik.webp";
 const testimonialsData = [
   {
     name: "Tejash Patel",
@@ -52,5 +61,61 @@ const testimonialsData = [
     image: preyash,
     rating: 5,
   },
+  {
+    name: "Patel Drashti Manojbhai",
+    feedback:
+      " I had a great experience taking this class. It's very easy to understand and practical. Every week, I have learned more and more, and it helps me improve my speaking a lot. Thank you so much, Ami Teacher.",
+    image: dhrasti,
+    rating: 4.5,
+  },
+  {
+    name: "Feni Patel",
+    feedback:
+      "Modern English classes are amazing for learning grammar and spoken English. If you want to improve your English, I liked the teaching by Ami Ma'am.",
+    image: feni,
+    rating: 5,
+  },
+  {
+    name: "Hemanshi Dhandhukiya ",
+    feedback:
+      "It has been a wonderful experience to join Modern English Classes. Ami Ma'am teaches English in a very friendly manner. I have greatly improved my English, and it has been very helpful for my studies.",
+    image: hemanshi,
+    rating: 5,
+  },
+  {
+    name: "Nilesh Patel ",
+    feedback:
+      "I really like the way Ami Ma'am teaches the students and pays attention to each student. She is a very kind, polite, and cooperative person. If you really want to improve your English, then you must join Modern English.",
+    image: nilesh,
+    rating: 5,
+  },
+  {
+    name: "Disha Patel ",
+    feedback: "She is friendly and teaches very well",
+    image: jayshree,
+    rating: 5,
+  },
+  {
+    name: "Yogi patel ",
+    feedback:
+      "It's an outstanding experience with this class. I learned so many things",
+    image: yogesh,
+    rating: 5,
+  },
+  {
+    name: "Yug Kishorbhai Dhandhukiya ",
+    feedback:
+      "Recently I joined this class, and it was a great experience with Ami Ma'am. The way she teaches, any student can understand easily. Billion stars to this class.",
+    image: yug,
+    rating: 5,
+  },
+  {
+    name: "Muaja Malik ",
+    feedback:
+      "Modern English classes are amazing for learning grammar and spoken English. If you want to improve your English, I highly recommend Ami Ma'am's teaching.",
+    image: malik,
+    rating: 5,
+  },
+  
 ];
 export default testimonialsData;
